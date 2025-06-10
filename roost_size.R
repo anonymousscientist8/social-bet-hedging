936afece182a9419ff2b8bcb6bac7b9a5f8e99a8
anonymousscientist8 <133162442+anonymousscientist8@users.noreply.github.com>
2025-06-10 10:13:42 -0500

    Update roost_size.R

--- roost_size.R
+++ roost_size.R
@@ -3,15 +3,16 @@ rm(list = ls())
 library(tidyverse)
 
 # import data
-d <- read.csv("C:\\Users\\raven\\Documents\\empirical.csv")
+d <- read.csv("filename\\empirical.csv")
 
-# for each roost in each simulation
+# for each roost in each simulation, get average roost size
 avg <- rep(0, length(d[1,]))
 for (i in 1:length(d[1,])) {
   temp <- d[d[,i] != 0,]
   avg[i] <- mean(temp[,i])
 }
 
+# Find the average roost size across all 6 scenarios
 mean(avg[1:60], na.rm = T)
 mean(avg[61:120],na.rm = T)
 mean(avg[121:180], na.rm = T)
